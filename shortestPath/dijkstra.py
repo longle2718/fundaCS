f7be9a5222fbdd5b23c95b3696918116fe9f5c56
Long Le <longle1@illinois.edu>
2016-12-30 20:22:03 -0600

    implemented dijkstra

--- shortestPath/dijkstra.py
+++ shortestPath/dijkstra.py
@@ -4,61 +4,68 @@
 # University of Illinois
 #
 import numpy as np
-import queue
+from pqdict import minpq
 
-define dijkstra(aMap,src):
+def dijkstra(aMap,start,goal):
     # basic, priority-queue version
     M,N = np.shape(aMap)
 
-    unexploredQ = queue.PriorityQueue() # inf queue of unexplored nodes
-    dist = {}
-    prev = {}
-    for m in range(M)dd:
+    # the queue of all untouched nodes
+    # dict/map with queue features (pop/additem)
+    # smaller value has higher priority
+    qDist = minpq() 
+    # dict/map
+    prev = {} 
+    for m in range(M):
         for n in range(N):
-            if [m,n] == src:
-                dist[(m,n)] = 0
+            if [m,n] == start:
+                qDist[(m,n)] = 0
             else:
-                dist[(m,n)] = np.infty
+                qDist[(m,n)] = np.infty
             prev[(m,n)] = np.nan
-            unexploredQ.put((dist[(m,n)],[m,n]))
 
-    while !unexploredQ.empty():
-        u = unexploredQ.get()
-        for n in getNeighbor(u):
-            if vecExist(n,unexploredQ.queue):
-                alt = dist[tuple(u)] + getDist(u,v)
-                if alt < dist[tuple(v)]:
-                    dist[tuple(v)] = alt
-                    prev[tuple(v)] = u
+    while len(qDist) > 0:
+        u = qDist.pop()
+        if u == goal:
+            return getPath(u,prev) 
+        for v in getNeighbor(u,M,N):
+            if v in qDist:
+                alt = qDist[u] + getDist(u,v)
+                if alt < qDist[v]:
+                    qDist[v] = alt
+                    prev[v] = u
     
     return None
 
-define dijkstra_ucs(aMap,src):
+def dijkstra_ucs(aMap,start,goal):
     # uniform cost search
 
     return None
 
-define Astar(aMap,src):
+def Astar(aMap,start,goal):
     # A* improvement
 
     return None
 
-def getDist(u,v):
-    return np.linalg.norm(u-v)
+def getPath(cur,prev):
+    fullPath = []
+    fullPath.append(cur)
+    while cur in prev:
+        cur = prev[cur]
+        fullPath.append(cur)
 
-def getNeighbor(src,M,N):
+    return fullPath
+
+def getDist(u,v):
+    return np.linalg.norm(np.array(u)-np.array(v))
+
+def getNeighbor(node,M,N):
     ngb = []
     delta = [[0,1],[1,0],[1,1],[0,-1],[-1,0],[-1,-1],[1,-1],[-1,1]]
     for d in delta:
-        n = src+d
+        n = list(node)+d
         if n[0]>=0 and n[0]<M and n[1]>=0 and n[1]<N:
-            ngb.append(n)
+            ngb.append(tuple(n))
 
     return ngb
 
-def vecExist(vec,aQueue):
-    for vecQ in aQueue:
-        if vec == vecQ: # assume list, not np.array
-            return True
-
-    return False
