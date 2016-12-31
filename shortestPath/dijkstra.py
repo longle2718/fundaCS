85a7dfb2291acb26d84f20c9820d2107b001d10f
Long Le <longle1@illinois.edu>
2016-12-30 23:35:51 -0600

    debugged dijkstra

--- shortestPath/dijkstra.py
+++ shortestPath/dijkstra.py
@@ -10,29 +10,39 @@ def dijkstra(aMap,start,goal):
     # basic, priority-queue version: only works for finite maps
     M,N = np.shape(aMap)
 
-    # one queue of all untouched nodes
+    # one queue of all untouched nodes (hence only need to dequeue)
     # dict/map with queue features (pop/additem)
     # smaller value has higher priority
-    qDist = minpq() 
-    # dict/map
+    Q = minpq() 
+    # dict/map, for storing results
+    dist = {}
     prev = {} 
+
     for m in range(M):
         for n in range(N):
-            if [m,n] == start:
-                qDist[(m,n)] = 0
+            node = (m,n)
+            if node == start:
+                Q[node] = 0
             else:
-                qDist[(m,n)] = np.infty
+                Q[node] = np.infty
 
-    while len(qDist) > 0:
-        u = qDist.pop()
-        if u == goal:
-            return getPath(u,prev) 
-        for v in getNeighbor(u,M,N):
-            if v in qDist:
-                alt = qDist[u] + getDist(u,v)
-                if alt < qDist[v]:
-                    qDist[v] = alt
-                    prev[v] = u
+    while len(Q) > 0:
+        node,minD = Q.popitem()
+        #print('node = '+str(node))
+        dist[node] = minD
+        if node == goal:
+            #print('dist = '+str(dist))
+            #print('prev = '+str(prev))
+            return getPath(node,prev), dist[node] 
+
+        for ngb in getNeighbor(node,M,N):
+            #print('ngb = '+str(ngb))
+            if ngb in Q:
+                alt = dist[node] + getDist(node,ngb,aMap) # node is already out of Q
+                if alt < Q[ngb]:
+                    #print('alt = '+str(alt))
+                    Q[ngb] = alt
+                    prev[ngb] = node
     
     return None
 
@@ -41,7 +51,7 @@ def dijkstra_ucs(aMap,start,goal):
     M,N = np.shape(aMap)
 
     # two queues: 
-    # a queue of growing untouched nodes
+    # a queue of growing/incomplete untouched nodes
     # a queue of visited nodes
 
 
@@ -61,14 +71,17 @@ def getPath(cur,prev):
 
     return fullPath
 
-def getDist(u,v):
+def getDist(u,v,aMap):
+    if aMap[u] == 1 or aMap[v] == 1:
+        return np.infty
+
     return np.linalg.norm(np.array(u)-np.array(v))
 
 def getNeighbor(node,M,N):
     ngb = []
     delta = [[0,1],[1,0],[1,1],[0,-1],[-1,0],[-1,-1],[1,-1],[-1,1]]
     for d in delta:
-        n = list(node)+d
+        n = np.array(node)+d # list and dict just append elements, only np.array adds/mults element-wise
         if n[0]>=0 and n[0]<M and n[1]>=0 and n[1]<N:
             ngb.append(tuple(n))
 
