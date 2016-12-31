15e56e2836e98795a33f6070f210d17e18a3f966
Long Le <longle1@illinois.edu>
2016-12-31 00:24:36 -0600

    debugged dijkstra_ucs

--- shortestPath/dijkstra.py
+++ shortestPath/dijkstra.py
@@ -10,38 +10,40 @@ def dijkstra(aMap,start,goal):
     # basic, priority-queue version: only works for finite maps
     M,N = np.shape(aMap)
 
-    # one queue of all untouched nodes (hence only need to dequeue)
-    # dict/map with queue features (pop/additem)
-    # smaller value has higher priority
-    Q = minpq() 
     # dict/map, for storing results
     dist = {}
     prev = {} 
+    # one queue of all untouched nodes (hence only need to dequeue)
+    # dict/map with queue features (pop/additem)
+    # smaller value has higher priority
+    unexplored = minpq() 
 
     for m in range(M):
         for n in range(N):
             node = (m,n)
             if node == start:
-                Q[node] = 0
+                unexplored[node] = 0
             else:
-                Q[node] = np.infty
+                unexplored[node] = np.infty
 
-    while len(Q) > 0:
-        node,minD = Q.popitem()
+    while len(unexplored) > 0:
+        # dequeue
+        node,minD = unexplored.popitem()
         #print('node = '+str(node))
         dist[node] = minD
         if node == goal:
             #print('dist = '+str(dist))
             #print('prev = '+str(prev))
-            return getPath(node,prev), dist[node] 
+            return getPath(node,prev),dist[node]
 
+        # visit neighbors
         for ngb in getNeighbor(node,M,N):
             #print('ngb = '+str(ngb))
-            if ngb in Q:
-                alt = dist[node] + getDist(node,ngb,aMap) # node is already out of Q
-                if alt < Q[ngb]:
+            if ngb in unexplored:
+                alt = dist[node] + getDist(node,ngb,aMap) # node is already out of unexplored
+                if alt < unexplored[ngb]:
                     #print('alt = '+str(alt))
-                    Q[ngb] = alt
+                    unexplored[ngb] = alt
                     prev[ngb] = node
     
     return None
@@ -50,10 +52,41 @@ def dijkstra_ucs(aMap,start,goal):
     # uniform cost search: works for inf maps
     M,N = np.shape(aMap)
 
+    # dict/map, for storing results
+    dist = {}
+    prev = {} 
     # two queues: 
     # a queue of growing/incomplete untouched nodes
+    frontier = minpq()
     # a queue of visited nodes
+    explored = []
 
+    frontier[start] = 0
+
+    while True:
+        if len(frontier) == 0:
+            return None
+
+        # dequeue
+        node,minD = frontier.popitem()
+        dist[node] = minD
+        #print('minD = '+str(minD))
+        if node == goal:
+            return getPath(node,prev),dist[node]
+
+        explored.append(node)
+
+        # visit neighbors
+        for ngb in getNeighbor(node,M,N):
+            #print('ngb = '+str(ngb))
+            if ngb not in explored:
+                if ngb not in frontier:
+                    frontier[ngb] = np.infty # new frontier
+
+                alt = dist[node] + getDist(node,ngb,aMap)
+                if alt < frontier[ngb]:
+                    frontier[ngb] = alt
+                    prev[ngb] = node
 
     return None
 
