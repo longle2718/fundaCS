80503e850aa1c3e2144249ba0d0d4a6030d1dded
Long Le <longle2718@gmail.com>
2018-05-06 22:08:28 -0500

    added dijkstra_pqdict(),dijkstra_ucs_pqdict(),Astar_pqdict()

--- shortestPath/dijkstra.py
+++ shortestPath/dijkstra.py
@@ -6,8 +6,9 @@
 import numpy as np
 from pqdict import minpq
 
-def dijkstra(aMap,start,goal):
+def dijkstra_pqdict(aMap,start,goal):
     # basic, priority-queue version: only works for finite maps
+    # version with pqdict
     M,N = np.shape(aMap)
 
     # dict/map, for storing results
@@ -48,6 +49,56 @@ def dijkstra(aMap,start,goal):
     
     return None,None
 
+def dijkstra(aMap,start,goal):
+    # basic, priority-queue version: only works for finite maps
+    # version without pqdict
+    M,N = np.shape(aMap)
+
+    # dict/map, for storing results
+    dist = {}
+    prev = {} 
+    # one priority (a dict/map hybrid with queue features, i.e. pop/additem) queue 
+    # of all untouched nodes (hence only need to dequeue)
+    # smaller value has higher priority
+    unexplored = []
+    unexploredScore = [] 
+
+    for m in range(M):
+        for n in range(N):
+            node = (m,n)
+            if node == start:
+                unexplored.append(node)
+                unexploredScore.append(0)
+            else:
+                unexplored.append(node)
+                unexploredScore.append(np.infty)
+
+    while len(unexplored) > 0:
+        # dequeue
+        popIdx = unexploredScore.index(min(unexploredScore))
+        minD = unexploredScore.pop(popIdx)
+        node = unexplored.pop(popIdx)
+        dist[node] = minD
+        #print('dist[node]= '+str(dist[node]))
+        if node == goal:
+            #print('dist = '+str(dist))
+            #print('prev = '+str(prev))
+            return getPath(node,prev),dist[node]
+
+        # visit neighbors
+        for ngb in getNeighbor(node,M,N):
+            #print('ngb = '+str(ngb))
+            if ngb in unexplored:
+                # index into dist since node is already out of unexplored
+                alt = dist[node] + getDist(node,ngb,aMap) 
+                ngbIdx = unexplored.index(ngb)
+                if alt < unexploredScore[ngbIdx]:
+                    #print('alt = '+str(alt))
+                    unexploredScore[ngbIdx] = alt
+                    prev[ngb] = node
+    
+    return None,None
+
 # without a priority queue, can only dequeue the first-come node (which is the same as the nearest node for uniform edge)
 # BFS the whole map
 def dijkstra_bfs(aMap,start,goal):
@@ -81,7 +132,7 @@ def dijkstra_bfs(aMap,start,goal):
     return getPath(goal,prev),dist[goal],explored
 
 # with a priority queue, greedy (dequeuing the nearest node first) is optimal
-def dijkstra_ucs(aMap,start,goal):
+def dijkstra_ucs_pqdict(aMap,start,goal):
     # uniform cost search: works for inf maps
     M,N = np.shape(aMap)
 
@@ -91,11 +142,11 @@ def dijkstra_ucs(aMap,start,goal):
     # two queues: 
     # a priority (dict-hybrid) queue of growing/incomplete untouched nodes
     frontier = minpq()
+    frontier[start] = 0
     # a queue of visited nodes
     #explored = []
     explored = set()
 
-    frontier[start] = 0
 
     while len(frontier) > 0:
         # dequeue
@@ -123,21 +174,70 @@ def dijkstra_ucs(aMap,start,goal):
 
     return None,None,explored
 
-def Astar(aMap,start,goal):
+def dijkstra_ucs(aMap,start,goal):
+    # uniform cost search: works for inf maps
+    M,N = np.shape(aMap)
+
+    # dict/map, for storing results
+    dist = {}
+    prev = {} 
+    # two queues: 
+    # a priority (dict-hybrid) queue of growing/incomplete untouched nodes
+    frontier = []
+    frontier.append(start)
+    frontierScore = []
+    frontierScore.append(0)
+    # a queue of visited nodes
+    #explored = []
+    explored = set()
+
+
+    while len(frontier) > 0:
+        # dequeue
+        popIdx = frontierScore.index(min(frontierScore))
+        minD = frontierScore.pop(popIdx)
+        node = frontier.pop(popIdx)
+        dist[node] = minD
+        #print('dist[node]= '+str(dist[node]))
+        if node == goal:
+            return getPath(node,prev),dist[node],explored
+
+        # enqueue
+        #explored.append(node)
+        explored.add(node)
+
+        # visit neighbors
+        for ngb in getNeighbor(node,M,N):
+            #print('ngb = '+str(ngb))
+            if ngb not in explored:
+                if ngb not in frontier:
+                    # new frontier
+                    frontier.append(ngb)
+                    frontierScore.append(np.infty)
+
+                alt = dist[node] + getDist(node,ngb,aMap)
+                ngbIdx = frontier.index(ngb)
+                if alt < frontierScore[ngbIdx]:
+                    frontierScore[ngbIdx] = alt
+                    prev[ngb] = node
+
+    return None,None,explored
+
+def Astar_pqdict(aMap,start,goal):
     # A* improvement: works for inf maps and efficient
     M,N = np.shape(aMap)
 
     # dict/map, for storing results
     dist = {} # mutable in A*
+    dist[start] = 0
     prev = {}
     # two queues: 
     # a priority (dict-hybrid) queue of growing/incomplete untouched nodes
     frontier = minpq()
+    frontier[start] = 0+getHeuristic(start,goal)
     # a queue of visited nodes
     explored = set()
 
-    frontier[start] = 0+getHeuristic(start,goal)
-    dist[start] = 0
 
     while len(frontier) > 0:
         # dequeue
@@ -161,6 +261,56 @@ def Astar(aMap,start,goal):
                 alt = dist[node] + getDist(node,ngb,aMap)
                 if alt < dist[ngb]:
                     frontier[ngb] = alt+getHeuristic(ngb,goal)
+                    dist[ngb] = alt
+                    prev[ngb] = node
+
+    return None,None,explored
+
+def Astar(aMap,start,goal):
+    # A* improvement: works for inf maps and efficient
+    M,N = np.shape(aMap)
+
+    # dict/map, for storing results
+    dist = {} # mutable in A*
+    dist[start] = 0
+    prev = {}
+    # two queues: 
+    # a priority (dict-hybrid) queue of growing/incomplete untouched nodes
+    frontier = []
+    frontier.append(start)
+    frontierScore = []
+    frontierScore.append(0+getHeuristic(start,goal))
+    # a queue of visited nodes
+    explored = set()
+
+
+    while len(frontier) > 0:
+        # dequeue
+        popIdx = frontierScore.index(min(frontierScore))
+        frontierScore.pop(popIdx)
+        node = frontier.pop(popIdx)
+        #print('dist[node]= '+str(dist[node]))
+        # dist.get(node,np.infty) is equivalent to dist[node] with the default value of np.infty 
+        if node == goal:
+            return getPath(node,prev),dist[node],explored
+
+        # enqueue
+        explored.add(node)
+
+        # visit neighbors
+        for ngb in getNeighbor(node,M,N):
+            #print('ngb = '+str(ngb))
+            if ngb not in explored:
+                if ngb not in frontier:
+                    # new frontier
+                    frontier.append(ngb)
+                    frontierScore.append(np.infty)
+                    dist[ngb] = np.infty
+
+                alt = dist[node] + getDist(node,ngb,aMap)
+                ngbIdx = frontier.index(ngb)
+                if alt < dist[ngb]:
+                    frontierScore[ngbIdx] = alt+getHeuristic(ngb,goal)
                     dist[ngb] = alt
                     prev[ngb] = node
 
