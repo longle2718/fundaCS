dc048db0fe8b616557bcbeeec6153d46d406115d
Long N Le <longn.le@samsung.com>
2018-05-17 07:32:35 -0700

    find all shortest paths in findLadders.py

--- shortestPath/findLadders.py
+++ shortestPath/findLadders.py
@@ -7,77 +7,97 @@ class Solution:
         :rtype: List[List[str]]
         """
         
-        dist = {}
-        dist[beginWord] = 0
-        prev = {}
+        prev = {} # word->prev word
         prev[beginWord] = None
         
-        frontier = []
-        frontier.append(beginWord)
-        frontierScore = []
-        frontierScore.append(0)
+        exploring = [] # exploring queue
+        exploring.append(beginWord)
+        exploringCost = [] # exploring cost so far
+        exploringCost.append(0)
         
-        explored = set()
-        while len(frontier) > 0:
+        explored = set() # explored queue
+        
+        while len(exploring) > 0:
             # dequeue
-            popIdx = frontierScore.index(min(frontierScore))
-            frontierScore.pop(popIdx)
-            word_cur = frontier.pop(popIdx)
-            print('word_cur = %s' % word_cur)
+            popIdx = exploringCost.index(min(exploringCost))
+            cost_cur = exploringCost.pop(popIdx)
+            word_cur = exploring.pop(popIdx)
+            if word_cur == endWord:
+                break
             
             # enqueue
             explored.add(word_cur)
             
-            # output
-            if word_cur == endWord:
-                print('endWord reached')
-                break
-            
-            # search neighbors
+            # exploring neighbors
             for word in wordList:
                 if word not in explored and self.isNeighbor(word,word_cur):
-                    if word not in dist:
-                        dist[word] = dist[word_cur] + 1
-                        prev[word] = word_cur
+                    if word not in exploring:
+                        exploring.append(word)
+                        exploringCost.append(cost_cur+1)
+                        prev[word] = [word_cur]
                     else:
-                        if dist[word] > dist[word_cur] + 1:
-                            dist[word] = dist[word_cur] + 1
-                            prev[word] = word_cur
-
-                    frontier.append(word)
-                    frontierScore.append(dist[word])
-                    
-     
+                        idx = exploring.index(word)
+                        if exploringCost[idx] > cost_cur+1:
+                            exploringCost[idx] = cost_cur+1
+                            prev[word] = [word_cur]
+                        elif exploringCost[idx] == cost_cur+1:
+                            prev[word].append(word_cur)
+        
         return self.getSeq(endWord,prev)
             
-    def getSeq(self,endWord,prev):
-        if endWord is None:
-            return []
+    def getSeq(self,endWord,prevSrc):
+        prev = dict(prevSrc)
+        #print('prev = %s' % prev)
         
-        word_cur = endWord
-        seq = []
-        while True:
-            seq.append(word_cur)
-            word_cur = prev[word_cur]
-            if word_cur is None:
-                break
+        # list of all seqs
+        seqList = []
         
-        return seq[::-1]
+        seqQueue = []
+        seqQueue.append([])
+        while len(seqQueue) > 0:
+            seq = seqQueue.pop()
+            # back track for a seq
+            if len(seq) == 0:
+                word_cur = endWord
+            else:
+                word_cur = seq.pop()
+            while True:
+                if word_cur not in prev:
+                    break
+                else:
+                    seq.append(word_cur)
+                    if prev[word_cur] is None:
+                        break
+                    elif len(prev[word_cur]) == 1:
+                        word_cur = prev[word_cur][0]
+                    elif len(prev[word_cur]) > 1:
+                        seqQueue.append(list(seq))
+                        word_cur = prev[word_cur].pop(0)
+                        #print('prev = %s' % prev)
+                    
+            if len(seq) > 0:
+                seqList.append(seq)
+        
+        for k in range(len(seqList)):
+            seqList[k] = seqList[k][::-1]
+            
+        return seqList
         
     def isNeighbor(self,word1,word2):
         if self.dist(word1,word2) == 1:
             return True
         else:
             return False
-        
+    
     def dist(self,word1,word2):
-        dist = 0
         N1 = len(word1)
         N2 = len(word2)
         
+        dist = 0
         for k in range(min(N1,N2)):
             if word1[k] != word2[k]:
-                dist += 1;
+                dist += 1
         dist += max(N1,N2)-min(N1,N2)
         
         return dist
+        
