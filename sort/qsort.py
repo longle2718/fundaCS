cefbb3111f90f36db3528a644bc3d526401aee14
Long Le <longle2718@gmail.com>
2018-05-23 22:21:11 -0500

    Update qsort.py

--- sort/qsort.py
+++ sort/qsort.py
@@ -39,6 +39,18 @@ def copy(A,iS,iE,B):
 	for k in range(iS,iE):
 		A[k] = B[k]
 
+def mergeTwoLists(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        if l1 and l2:
+            if l1.val > l2.val:
+                l1,l2 = l2,l1
+            l1.next = self.mergeTwoLists(l1.next,l2)
+        return l1 or l2
+
 #############################
 def pivotSort(A):
 	'''
